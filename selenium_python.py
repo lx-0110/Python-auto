3965485af285251f09361fac0f65c8b73915f5df
lx-0110 <80656214+lx-0110@users.noreply.github.com>
2021-03-15 13:34:29 +0800

    Add files via upload

---
+++ selenium_python.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.get('https://kaifa.17mine.cn/#/login')
+driver.maximize_window()
+sleep(2)
+
+driver.quit()
